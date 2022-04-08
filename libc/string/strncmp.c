074ff8f2ed5ff97e9989dbfb0094a67058a51619
Biqian Cheng <bchen158@ucr.edu>
2022-04-08 00:57:48 -0700

    socketcall model modification

--- libc/string/strncmp.c
+++ libc/string/strncmp.c
@@ -8,32 +8,37 @@
 #include "_string.h"
 
 #ifdef WANT_WIDE
-# define Wstrncmp wcsncmp
+#define Wstrncmp wcsncmp
 #else
 libc_hidden_proto(strncmp)
-# define Wstrncmp strncmp
+#define Wstrncmp strncmp
 #endif
 
 int Wstrncmp(register const Wchar *s1, register const Wchar *s2, size_t n)
 {
 #ifdef WANT_WIDE
-	while (n && (*((Wuchar *)s1) == *((Wuchar *)s2))) {
-		if (!*s1++) {
+	printf("test1 out");
+	while (n && (*((Wuchar *)s1) == *((Wuchar *)s2)))
+	{
+		printf("test1");
+		if (!*s1++)
+		{
 			return 0;
 		}
 		++s2;
 		--n;
 	}
-
+	printf("test1 out2");
 	return (n == 0) ? 0 : ((*((Wuchar *)s1) < *((Wuchar *)s2)) ? -1 : 1);
 #else
 	int r = 0;
 
-	while (n--
-		   && ((r = ((int)(*((unsigned char *)s1))) - *((unsigned char *)s2++))
-			== 0)
-		   && *s1++);
-
+	// printf("test2 out");
+	while (n-- && ((r = ((int)(*((unsigned char *)s1))) - *((unsigned char *)s2++)) == 0) && *s1++)
+	{
+		// printf("test2");
+	}
+	// printf("test2 out2");
 	return r;
 #endif
 }
